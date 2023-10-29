9d93dfea616ce90f8958e09da66801af4c4587b7
aladindev <kjune0205@naver.com>
2023-10-29 17:58:45 +0900

    kafka study

--- src/main/java/com/example/SimpleConsumer.java
+++ src/main/java/com/example/SimpleConsumer.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class SimpleConsumer {
     private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
@@ -49,15 +46,34 @@ public class SimpleConsumer {
 
                 // 개별 레코드 단위 매번 옵셋 커밋
                 //consumer.commitSync(currentOffset);
-                
+
                 // Async offset commit : 커밋이 완료될 때까지
                 // 응답을 기다리지 않기 때문에 동기 옵셋 커밋보다 동일 시간당 데이터 처리량이 더 많다.
                 consumer.commitAsync();
 
 
             }
+        }
+    }
 
+    // 컨슈머 내에 리밸런스가 일어났을 때 응답 주는 클래스
+    private static class RebalanceListener implements ConsumerRebalanceListener {
 
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+
+        }
+
+        @Override
+        public void onPartitionsLost(Collection<TopicPartition> partitions) {
+            ConsumerRebalanceListener.super.onPartitionsLost(partitions);
         }
     }
 }
+
+
