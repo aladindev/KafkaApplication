c71b217bdd3be1b48d9e846a274c28836d08147d
aladindev <kjune0205@naver.com>
2023-11-05 16:18:50 +0900

    kafka connect Stream

---
+++ src/main/java/com/example/FilterProcessor.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
+
+public class FilterProcessor implements Processor {
+
+    private ProcessorContext context;
+
+    @Override
+    public void init(ProcessorContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void close() {
+        Processor.super.close();
+    }
+
+    @Override
+    public void process(Record record) {
+        if(record.value().toString().length() > 5) {
+            context.forward(record, record.value().toString());
+        }
+        context.commit();
+    }
+}
