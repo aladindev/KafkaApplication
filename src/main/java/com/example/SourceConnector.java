c98e62c38c398787a1f9d2be5bf1acdb749e3e14
aladindev <kjune0205@naver.com>
2023-11-26 15:53:40 +0900

    Spring Kafka Application

--- src/main/java/com/example/SourceConnector.java
+++ src/main/java/com/example/SourceConnector.java
@@ -1,66 +1,68 @@
-package com.example;
-
-import org.apache.kafka.common.config.ConfigDef;
-import org.apache.kafka.connect.connector.Task;
-import org.apache.kafka.connect.source.ConnectorTransactionBoundaries;
-import org.apache.kafka.connect.source.ExactlyOnceSupport;
-import org.apache.kafka.connect.source.SourceConnectorContext;
-
-import java.util.List;
-import java.util.Map;
-
-public class SourceConnector extends org.apache.kafka.connect.source.SourceConnector {
-    public SourceConnector() {
-        super();
-    }
-
-    @Override
-    protected SourceConnectorContext context() {
-        return super.context();
-    }
-
-    @Override
-    public void start(Map<String, String> props) {
-
-    }
-
-    @Override
-    public Class<? extends Task> taskClass() {
-        return null;
-    }
-
-    @Override
-    public List<Map<String, String>> taskConfigs(int maxTasks) {
-        return null;
-    }
-
-    @Override
-    public void stop() {
-
-    }
-
-    @Override
-    public ConfigDef config() {
-        return null;
-    }
-
-    @Override
-    public ExactlyOnceSupport exactlyOnceSupport(Map<String, String> connectorConfig) {
-        return super.exactlyOnceSupport(connectorConfig);
-    }
-
-    @Override
-    public ConnectorTransactionBoundaries canDefineTransactionBoundaries(Map<String, String> connectorConfig) {
-        return super.canDefineTransactionBoundaries(connectorConfig);
-    }
-
-    @Override
-    public boolean alterOffsets(Map<String, String> connectorConfig, Map<Map<String, ?>, Map<String, ?>> offsets) {
-        return super.alterOffsets(connectorConfig, offsets);
-    }
-
-    @Override
-    public String version() {
-        return null;
-    }
-}
+//package com.example;
+//
+//import org.apache.kafka.common.config.Config;
+//import org.apache.kafka.common.config.ConfigDef;
+//import org.apache.kafka.connect.connector.ConnectorContext;
+//import org.apache.kafka.connect.connector.Task;
+//import org.apache.kafka.connect.source.ConnectorTransactionBoundaries;
+//import org.apache.kafka.connect.source.ExactlyOnceSupport;
+//import org.apache.kafka.connect.source.SourceConnectorContext;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public class SourceConnector extends org.apache.kafka.connect.source.SourceConnector {
+//    public SourceConnector() {
+//        super();
+//    }
+//
+//    @Override
+//    public void initialize(ConnectorContext ctx) {
+//        super.initialize(ctx);
+//    }
+//
+//    @Override
+//    public void initialize(ConnectorContext ctx, List<Map<String, String>> taskConfigs) {
+//        super.initialize(ctx, taskConfigs);
+//    }
+//
+//    @Override
+//    public void reconfigure(Map<String, String> props) {
+//        super.reconfigure(props);
+//    }
+//
+//    @Override
+//    public Config validate(Map<String, String> connectorConfigs) {
+//        return super.validate(connectorConfigs);
+//    }
+//
+//    @Override
+//    public void start(Map<String, String> props) {
+//
+//    }
+//
+//    @Override
+//    public Class<? extends Task> taskClass() {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Map<String, String>> taskConfigs(int maxTasks) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void stop() {
+//
+//    }
+//
+//    @Override
+//    public ConfigDef config() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String version() {
+//        return null;
+//    }
+//}
---
+++ src/main/java/com/example/kafka/application/SpringKafkaApplication.java
@@ -0,0 +1,66 @@
+package com.example.kafka.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(SpringKafkaApplication.class);
+    private static String TOPIC_NAME = "test";
+
+    /** KafkaTemplate을 @Autowired 어노테이션으로 주입받아 사용
+     * 사용자가 직접 선언하지 않은 bean 객체지만
+     * 스프링 카프카에서 제공하는 객체로 주입된다.
+     * application.yaml에 선언한 옵션값은 자동으로 주입된다.*/
+    @Autowired
+    private KafkaTemplate<String, String> customKafkaTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringKafkaApplication.class);
+        application.run(args);
+    }
+
+//    /** Kafka Listener */
+//    @KafkaListener(topics = "test", groupId = "test-group-00")
+//    public void recordListener(ConsumerRecord<String, String> record) {
+//        logger.info(record.toString());
+//    }
+//    @KafkaListener(topics = "test", groupId = "test-group-01")
+//    public void singleTopicListener(String messageValue) {
+//        logger.info(messageValue);
+//    }
+//    @KafkaListener(topics = "test", groupId = "test-group-02", properties = {"max.poll.interval.ms:60000", "auto.offset.reset:earliest"})
+//    public void singleTopicWithPropertiesListener(String messageValue) {
+//        logger.info(messageValue);
+//    }
+//    @KafkaListener(topicPartitions = {
+//             @TopicPartition(topic = "test01", partitions = {"0", "1"})
+//            ,@TopicPartition(topic = "test02", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "3"))
+//    })
+
+/* 커맨드 라인으로 실행할 시 */
+//    @Override
+//    public void run(String... args) throws Exception {
+//        ListenableFuture<SendResult<String, String>> future = customKafkaTemplate.send(TOPIC_NAME, "test");
+//        future.addCallback(new KafkaSendCallback<String, String>() {
+//            @Override
+//            public void onSuccess(SendResult<String, String> result) {
+//
+//            }
+//            @Override
+//            public void onFailure(Throwable ex) {
+//                KafkaSendCallback.super.onFailure(ex);
+//            }
+//            @Override
+//            public void onFailure(KafkaProducerException ex) {
+//
+//            }
+//        });
+//        System.exit(0); // 작업 완료 후 종료
+//    }
+}
