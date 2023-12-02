5aa35967cfe50e1d34e94ce792594a0ef22352af
aladindev <kjune0205@naver.com>
2023-12-02 15:06:36 +0900

    Spring Kafka Hadoop Elastic Search Application

--- src/main/java/com/pipeline/config/ElasticSearchSinkConnectorConfig.java
+++ src/main/java/com/pipeline/config/ElasticSearchSinkConnectorConfig.java
@@ -8,6 +8,19 @@ import java.util.Map;
 public class ElasticSearchSinkConnectorConfig extends AbstractConfig {
 
     public static final String ES_CLUSTER_HOST = "ex.host";
+    private static final String ES_CLUSTER_HOST_DEFAULT_VALUE = "localhost";
+    private static final String ES_CLUSTER_HOST_DOC = "엘라스틱서치 호스트를 입력";
+
+    public static final String ES_CLUSTER_PORT = "es.port";
+    private static final String ES_CLUSTER_PORT_DEFAULT_VALUE = "9200";
+    private static final String ES_CLUSTER_PORT_DOC = "엘라스틱서치 포트를 입력";
+
+    public static final String ES_INDEX = "ex.index";
+    private static final String ES_INDEX_DEFAULT_VALUE = "kafka-connector-index";
+    private static final String ES_INDEX_DOC = "엘라스틱서치 인덱스를 입력";
+
+    
+
     public ElasticSearchSinkConnectorConfig(ConfigDef definition, Map<?, ?> originals) {
         super(definition, originals);
     }
